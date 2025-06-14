0bee707c1727f5a2fce0fc9d1080583afea3cdff
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-06-14 14:08:38 +0530

    Time: 0 ms (100%), Space: 13.2 MB (48.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,17 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        arr = []
-        # i = 0
-        j = 1
+        
         mapp = {}
-        for i in range(len(nums)-1):
-            if nums[i]+nums[j]==target:
-                return [i,j]
-            elif target-nums[i] in mapp:
+        for i in range(len(nums)):
+            
+            if target-nums[i] in mapp:
                 return [mapp[target-nums[i]],i]
             mapp[nums[i]] = i
-            j+=1
-        if target-nums[len(nums)-1] in mapp:
-            return [mapp[target-nums[len(nums)-1]],len(nums)-1]
-        return arr
+            
+        return -1
