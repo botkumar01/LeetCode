e414916618cce400db92b0a56b382a1b8eb15da5
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-06-15 18:46:05 +0530

    Time: 188 ms (31.64%), Space: 14.9 MB (9.11%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def numberOfSubstrings(self, nums):
+        """
+        :type s: str
+        :rtype: int
+        """
+        cnt = 0
+        aa =0 
+        bb=0
+        cc=0
+        a=[]
+        b = []
+        c = []
+        for i in range(len(nums)):
+            if nums[i] == "a":
+                a.append(i)
+            elif nums[i] == "b":
+                b.append(i)
+            else:
+                c.append(i)
+        for i in nums:
+            if aa>=len(a) or  bb>=len(b) or  cc>=len(c):
+                                break
+            end = max(a[aa],b[bb],c[cc])
+            cnt += len(nums)-end
+            if i =='a':
+                aa+=1
+            elif i=='b':
+                bb+=1
+            else:
+                cc+=1
+
+            
+            
+        return cnt
+        # for i in range(len(nums)-2):
+        #     a = False
+        #     b=False
+        #     c = False
+        #     for j in range(i,len(nums)):
+        #         if nums[j] == "a":
+        #             a = True
+        #         elif nums[j] == "b":
+        #             b = True
+        #         else:
+        #             c =True
+        #         if a==True and b ==True and c ==True:
+        #             cnt += len(nums)-j
+        #             break
+        # return cnt
+    
