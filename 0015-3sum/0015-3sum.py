58c80198fecd234edcb5b7a945169a59716ba073
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-07-28 23:18:25 +0530

    Time: 5774 ms (9.09%), Space: 19 MB (21.56%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -11,14 +11,19 @@ class Solution(object):
         for i in range(len(nums)-1):
             l=i+1
             r = len(nums)-1
+            if nums[i] > 0 :
+                break
             while l<r:
-                if nums[i] + nums[l]+nums[r] ==0:
+                t =nums[i] + nums[l]+nums[r]
+                # if nums[i]>0:
+                #     continue
+                if t ==0:
                     # if ans[-1] != [nums[i],nums[l],nums[r]]:
                     if [nums[i],nums[l],nums[r]] not in ans:
                         ans.append([nums[i],nums[l],nums[r]])
                     l+=1
                     r-=1
-                elif nums[i] + nums[l]+nums[r] < 0:
+                elif t < 0:
                     l+=1
                 else:
                     r-=1
