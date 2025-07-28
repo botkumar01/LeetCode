18c1ad20b2540a84b3b2ce38416d167426a1a7b8
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-07-29 01:26:53 +0530

    Time: 15 ms (24%), Space: 17.2 MB (8.48%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def merge(self, arr):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # cnt = 0
+        # if len(arr)<=1:
+        #     return arr
+        # ans = []
+        # arr.sort()
+        # # print(arr)
+        # a = arr[0][0]
+        # b = arr[0][1]
+        # for i in range(1,len(arr)):
+        #     if b >= arr[i][0]:
+        #         b = max(b,arr[i][1])
+        #     else:
+        #         # cnt +=1
+        #         ans.append([a,b])
+        #         a = arr[i][0]
+        #         b = max(arr[i][1],arr[i-1][1])
+        # ans.append([a,b])
+        # return(ans)
+        arr.sort()
+        ans = []
+        p = 0
+        l=0
+        while p < len(arr):
+            add = []
+            add.append(arr[p][0])
+            m=arr[p][1]
+            if p==len(arr)-1:
+                ans.append(arr[p])
+                break
+            while p<len(arr)-1 and m>= arr[p+1][0]:
+                m  = max(m,arr[p+1][1])
+                p+=1
+            add.append(m)
+            ans.append(add)
+            p+=1
+        return ans
+        # ans = [[arr[0]]]
+        # p=0
+        # for i in range(len(arr)-1):
+        #     while p < len(arr) and arr[p][0]>:
+        #         p+=1
+            
+        # return ans
